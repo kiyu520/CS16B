6b4b66edb30984cf1019685b745a70355d09f4e9
kiyu520 <2186925409@qq.com>
2025-09-19 14:57:59 +0800

    set homework'name to MIT

---
+++ MIT/test.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class test{
+	public  static void main(String[] ars){
+		List<String> lst = new ArrayList<>();
+lst.add("zero");
+lst.add("one");
+lst.set(0, "zed");
+System.out.println(lst.get(0));
+System.out.println(lst.size());
+if (lst.contains("one")) {
+    System.out.println("one in lst");
+}
+for (String elem : lst) {
+    System.out.println(elem);
+}	
+}
+}
